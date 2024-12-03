7dce48ce775a9f56fc8905f844aebfd13c85a5d9
rafaelgdgs <rafagsique@hotmail.com>
2024-12-03 18:42:05 -0300

    feat: basic 0.0.1 version of board and cell

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,40 @@ use std::{thread, time};
 // if LIVE and neighbours > 3 = DIE
 // if DEAD and neighbours == 3 = LIVE
 
+enum CellState {
+    Live,
+    Dead,
+}
+
+struct Cell {
+    state: CellState,
+    x: u32,
+    y: u32,
+}
+
+impl Cell {
+    fn play(&self, neighbours: u8) -> CellState {
+        match self.state {
+            CellState::Live => match neighbours {
+                2 | 3 => CellState::Live,
+                _ => CellState::Dead,
+            },
+            CellState::Dead => match neighbours {
+                3 => CellState::Live,
+                _ => CellState::Dead,
+            },
+        }
+    }
+}
+
+struct Game {
+    grid: Vec<Cell>,
+}
+
+impl Game {
+    fn run(&self) {}
+}
+
 #[macroquad::main("Game of Life")]
 async fn main() {
     println!("Hello, world!");

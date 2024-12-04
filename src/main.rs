647d7adeb4374ef7e77f3eeb209248b44686a39c
rafaelgdgs <rafagsique@hotmail.com>
2024-12-04 02:57:45 -0300

    feat: basic game loop and pause function, draws to the screen
    Problem on the click to change cell function, at least it compiles :)

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,21 @@ use std::{thread, time};
 // if LIVE and neighbours > 3 = DIE
 // if DEAD and neighbours == 3 = LIVE
 
+const GRID_SIZE: usize = 30;
+const CELL_SIZE: f32 = 10f32;
+
+trait Draw {
+    fn draw(&self);
+}
+
 enum CellState {
     Live,
     Dead,
 }
 
-struct Cell {
-    state: CellState,
-    x: u32,
-    y: u32,
-}
-
-impl Cell {
-    fn play(&self, neighbours: u8) -> CellState {
-        match self.state {
+impl CellState {
+    fn process(&self, neighbours: u8) -> CellState {
+        match self {
             CellState::Live => match neighbours {
                 2 | 3 => CellState::Live,
                 _ => CellState::Dead,
@@ -32,18 +33,115 @@ impl Cell {
     }
 }
 
+struct Cell {
+    state: CellState,
+    pos: Vec2,
+}
+
+impl Draw for Cell {
+    fn draw(&self) {
+        let color = match self.state {
+            CellState::Live => BLACK,
+            CellState::Dead => WHITE,
+        };
+        draw_rectangle_lines(
+            self.pos.x - 1f32,
+            self.pos.y - 1f32,
+            CELL_SIZE + 2f32,
+            CELL_SIZE + 2f32,
+            2f32,
+            GRAY,
+        );
+        draw_rectangle(self.pos.x, self.pos.y, CELL_SIZE, CELL_SIZE, color);
+        // draw_rectangle_lines(x, y, w, h, thickness, color);
+    }
+}
+
 struct Game {
-    grid: Vec<Cell>,
+    grid_current: Vec<Cell>,
+    paused: bool,
 }
 
 impl Game {
-    fn run(&self) {}
+    fn new() -> Self {
+        let mut grid: Vec<Cell> = Vec::new();
+        for i in 0..GRID_SIZE {
+            for j in 0..GRID_SIZE {
+                let cell = Cell {
+                    state: CellState::Dead,
+                    pos: Vec2 {
+                        x: (i * 10) as f32,
+                        y: (j * 10) as f32,
+                    },
+                };
+                grid.push(cell);
+            }
+        }
+        Self {
+            grid_current: grid,
+            paused: false,
+        }
+    }
+
+    fn run(&mut self) {
+        if is_mouse_button_pressed(MouseButton::Right) {
+            self.paused = !self.paused;
+        }
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let pos = mouse_position();
+            let cell = self
+                .grid_current
+                .get_mut((pos.1 / CELL_SIZE + pos.0) as usize);
+            match cell {
+                None => {}
+                Some(x) => x.state = CellState::Live,
+            }
+        }
+        if self.paused == true {
+            draw_text("PAUSED", 500f32, 500f32, 30f32, BLACK);
+            return;
+        }
+        let mut grid_next_frame: Vec<Cell> = Vec::new();
+        for line in 0..GRID_SIZE {
+            for column in 0..GRID_SIZE {
+                let cell_pos: Vec2 = match self.grid_current.get(line * GRID_SIZE + column) {
+                    None => {
+                        continue;
+                    }
+                    Some(v) => v.pos.clone(),
+                };
+                let c: Cell = Cell {
+                    // state: cell.state.process(get_neighbours(cell)),
+                    state: CellState::Dead,
+                    pos: cell_pos,
+                };
+                grid_next_frame.push(c);
+            }
+        }
+        self.grid_current = grid_next_frame;
+    }
+
+    // fn get_neighbours(&self, cell: &Cell) -> u8 {
+    //     if self.grid_current[0] {}
+    // }
+}
+
+impl Draw for Game {
+    fn draw(&self) {
+        for cell in &self.grid_current {
+            cell.draw();
+        }
+    }
 }
 
 #[macroquad::main("Game of Life")]
 async fn main() {
     println!("Hello, world!");
+    let mut game = Game::new();
     loop {
+        clear_background(WHITE);
+        game.run();
+        game.draw();
         println!("FPS: {:.1}", get_fps());
         thread::sleep(time::Duration::from_millis(15));
         next_frame().await
